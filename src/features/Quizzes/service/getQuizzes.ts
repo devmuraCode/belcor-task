eeb6d48f7409fe70140dc562b507ce9848b410b8
devmuraCode <muradkusherbaev@gmail.com>
2024-01-25 00:51:56 +0500

    try catch and name function

--- src/features/Quizzes/service/getQuizzes.ts
+++ src/features/Quizzes/service/getQuizzes.ts
@@ -1,10 +1,12 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const getQuizzes = createAsyncThunk(
-  'products/fetchProducts',
-  async () => {
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const getQuizzes = createAsyncThunk('quizzes/getQuizzes', async () => {
+  try {
     const response = await axios.get('http://localhost:3001/quizzes');
     return response.data;
-  },
-);
+  } catch (error) {
+    console.log(error);
+  }
+});
